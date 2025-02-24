4f478e3cf77b499e5106735355383d5d414dfc89
niharika464 <niharikasinghgaur11@gmail.com>
2025-02-24 21:37:21 +0530

    list

---
+++ dict.py
@@ -0,0 +1,21 @@
+stationary=[]
+print(stationary)
+
+stationary=['blue pen','black pen','scale','pencil','eraser']
+print(stationary)
+
+stationary.append('sharpner')
+print(stationary)
+
+stationary.remove('scale')
+print(stationary)
+
+stationary.pop(3)
+print(stationary)
+
+stationary.sort()
+print(stationary)
+
+stationary.clear()
+print(stationary)
+
